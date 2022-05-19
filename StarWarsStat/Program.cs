32f85a743e8b0ee60be1c5bf605e0c6bdc22073d
Charlie Sesser Nylund <charlie.sessernylund@elev.ga.ntig.se>
2022-05-19 12:20:14 +0200

    last commit

--- StarWarsStat/Program.cs
+++ StarWarsStat/Program.cs
@@ -8,33 +8,54 @@ using System.Text.Json.Serialization;
 
 RestClient client = new RestClient("https://pokeapi.co/api/v2/");
 
-RestRequest request = new RestRequest("pokemon/charizard");
+Console.WriteLine("This is an engine where you can search up a pokemon.\n Then you will be able to see if that pokemon has a punch move");
+Console.WriteLine("Do not write any numbers and write the pokemon's name in lowercase");
+Console.WriteLine("Please write below which pokemon you would like to search up:");
+
+string chari = Console.ReadLine();
+
+// string chari = "ditto";
+
+RestRequest request = new RestRequest($"pokemon/{chari}");
 
 RestResponse response = await client.GetAsync(request);
 
 string content = response.Content;
 
-Pokemon charizard = JsonConvert.DeserializeObject<Pokemon>(content);
+Pokemon poke = JsonConvert.DeserializeObject<Pokemon>(content);
 
 Console.WriteLine(response.StatusCode);
 
-Console.WriteLine($"Name: {charizard.name}  id:{charizard.id}");
-Console.WriteLine("all of charizards moves");
+string status = response.StatusCode.ToString();
 
-for (int i = 0; i < charizard.moves.Count; i++)
+
+
+if (status == "OK")
 {
 
-    Console.WriteLine(charizard.moves[i].move.name);
 
-    // string answer = Console.ReadLine();
-    // bool isPunch = answer.Contains("punch");
 
-    // if (isPunch == true)
-    // {
+    Console.WriteLine($"Name: {poke.name}  id:{poke.id}");
+    Console.WriteLine($"all of {chari} punch moves");
 
-    //     Console.WriteLine(charizard.moves[i].move.name);
+    for (int i = 0; i < poke.moves.Count; i++)
+    {
 
-    // }
+        string answer = poke.moves[i].move.name;
+        bool isPunch = answer.Contains("punch");
+
+        if (isPunch == true)
+        {
+
+            Console.WriteLine(poke.moves[i].move.name);
+
+        }
+    }
+}
+else
+{
+    Console.WriteLine("please write a correct pokemon");
+
 }
 
 Console.ReadLine();
